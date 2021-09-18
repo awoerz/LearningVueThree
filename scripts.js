37014f0c28eacea020c6a94d3bd5689df59d39da
Adam Woerz <azreow@gmail.com>
2021-09-18 11:03:56 -0400

    updated const to var in script.js so that vue would load locally instead of as ES6 module

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-const list = Vue.createApp({
 
-    components: ['simple-form'],
-    methods: {
-        myFunction() {
-            console.log("click")
+document.addEventListener("DOMContentLoaded", function() {
+    var list = Vue.createApp({
+
+        components: ['simple-form'],
+        methods: {
+            myFunction() {
+                console.log("click")
+            }
         }
-    }
+    })
+    
+    list.component(InputField.name, InputField)
+    list.component(ListItem.name, ListItem)
+    list.component(SimpleForm.name, SimpleForm)
+    
+    
+    list.mount('#items-list')
 })
-
-list.component(InputField.name, InputField)
-list.component(ListItem.name, ListItem)
-list.component(SimpleForm.name, SimpleForm)
-
-
-list.mount('#items-list')
